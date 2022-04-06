87401fc8b12cb9acd3c0b2c9eb735c4ca6e899dc
Hadi-AL-Saden <hadialsaden@gmail.com>
2022-04-06 13:17:17 -0800

    2Time

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+let str = 'the car drove fast';
+
+
+let arr = str.split(" ");
+
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+}
+
+let str2 = arr.join(" ");
+console.log(str2);
+document.write(str2);
